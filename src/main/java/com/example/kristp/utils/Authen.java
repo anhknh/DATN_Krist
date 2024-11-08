b16d35b7b3fab29c10d4d6f52c1ea773327378aa
anhknh <kieuhoanganh1207@gmail.com>
2024-11-08 19:51:02 +0700

    HoangAnh commit login session

--- src/main/java/com/example/kristp/utils/Authen.java
+++ src/main/java/com/example/kristp/utils/Authen.java
@@ -1,7 +1,9 @@
 package com.example.kristp.utils;
 
+import com.example.kristp.entity.KhachHang;
 import com.example.kristp.entity.NhanVien;
 import com.example.kristp.repository.NhanVienRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,22 +13,37 @@ public class Authen {
 
 
     public static NhanVien nhanVien = null;
+    public  static KhachHang khachHang = null;
 
-    public static void clear() {
+    public static void clearNhanVien() {
         nhanVien = null;
+    }
+    public static void clearKhachHang() {
+        khachHang = null;
     }
 
 
-//    public static boolean isLogin(HttpSession session) {
-//        customer = (Customer) session.getAttribute("customer");
-//        return customer != null;
-//    }
+    public static boolean isLoginKhachHang(HttpSession session) {
+        khachHang = (KhachHang) session.getAttribute("KhachHang");
+        return khachHang != null;
+    }
+    public static boolean isLoginNhanVien(HttpSession session) {
+        nhanVien = (NhanVien)  session.getAttribute("");
+        return nhanVien != null;
+    }
 //
-//    public static String getRole(HttpSession session) {
-//        if (isLogin(session)) {
-//            return customer.getUserId().getRole();
-//        } else {
-//            return null; // or handle as needed if not logged in
-//        }
-//    }
+    public static String getRoleKhachHang(HttpSession session) {
+        if (isLoginKhachHang(session)) {
+            return khachHang.getTaiKhoan().getChucVu();
+        } else {
+            return null; // or handle as needed if not logged in
+        }
+    }
+    public static String getRoleNhanVien(HttpSession session) {
+        if (isLoginNhanVien(session)) {
+            return nhanVien.getTaiKhoan().getChucVu();
+        } else {
+            return null; // or handle as needed if not logged in
+        }
+    }
 }
